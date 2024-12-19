68cc1925df09796654c7c7e5840a1f6c608df4f5
Lavanya Jayabal <157089962+lavanyajayan0404@users.noreply.github.com>
2024-12-19 19:52:57 +0530

    Create CarController.java

---
+++ CarController.java
@@ -0,0 +1,41 @@
+package com.example.carmanagement.controller;
+
+import com.example.carmanagement.entity.Car;
+import com.example.carmanagement.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carService.getCarById(id);
+    }
+
+    @PostMapping
+    public Car addCar(@RequestBody Car car) {
+        return carService.addCar(car);
+    }
+
+    @PutMapping("/{id}")
+    public Car updateCar(@PathVariable Long id, @RequestBody Car car) {
+        return carService.updateCar(id, car);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+    }
+}
